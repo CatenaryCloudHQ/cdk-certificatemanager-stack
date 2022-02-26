b6457e29855581676e6299e1b8b20bafd7fc8955
Roman Naumenko <hi@catenary.cloud>
2022-02-26 13:10:48 -0500

    Update projen

--- .projenrc.js
+++ .projenrc.js
@@ -1,12 +1,25 @@
 const { awscdk } = require('projen');
 const project = new awscdk.AwsCdkTypeScriptApp({
-  cdkVersion: '2.1.0',
-  defaultReleaseBranch: 'main',
-  name: 'cdk-certificatemanager-stack',
 
-  // deps: [],                /* Runtime dependencies of this module. */
-  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
-  // devDeps: [],             /* Build dependencies for this module. */
-  // packageName: undefined,  /* The "name" in package.json. */
+  author: 'Catenary.Cloud',
+  authorAddress: 'hi@catenary.cloud',
+  defaultReleaseBranch: 'main',
+  description: 'CDK app to show Cloudformation parameters parsing',
+  cdkVersion: '2.8.0',
+  defaultReleaseBranch: 'main',
+  name: 'cdk-certificatemanager',
+  buildWorkflow: false,
+  rebuildBot: false,
+  releaseWorkflow: false,
+  mergify: false,
+  pullRequestTemplate: false,
+  projenUpgrade: false,
+  workflows: false,
+  github: false,
+  license: false,
+  staleBranches: false,
 });
+
 project.synth();
+
+
