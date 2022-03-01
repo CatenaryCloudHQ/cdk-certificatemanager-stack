de95e0ce952ffeb730c3101961e757c6c59aa5b1
Roman Naumenko <hi@catenary.cloud>
2022-02-28 23:57:26 -0500

    Update project

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# replace this
+# CDK parameters parsing 
+
+This is CDK app to show much easier it is to pass parameters in CDK app compared to native Cloudformation functions.
+
+# Deploy
+
+`npx projen` and then
+`cdk deploy --all`
+
+# Project boostrap
+
+This repository was created with `npx projen new awscdk-app-ts`
--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,28 @@
+
 import { App, Stack, StackProps } from 'aws-cdk-lib';
+import { CertificateValidation, Certificate } from 'aws-cdk-lib/aws-certificatemanager';
+import { HostedZone } from 'aws-cdk-lib/aws-route53';
 import { Construct } from 'constructs';
 
-export class MyStack extends Stack {
+
+export class CertValidation extends Stack {
   constructor(scope: Construct, id: string, props: StackProps = {}) {
     super(scope, id, props);
 
-    // define resources here...
+    const myHostedZone = new HostedZone(this, 'HostedZone', {
+      zoneName: 'thesite.com',
+    });
+
+    new Certificate(this, 'Certificate', {
+      domainName: 'dev.thesite.com',
+      subjectAlternativeNames: [],
+      validation: CertificateValidation.fromDns(myHostedZone),
+    });
   }
 }
 
-// for development, use account/region from cdk cli
-const devEnv = {
-  account: process.env.CDK_DEFAULT_ACCOUNT,
-  region: process.env.CDK_DEFAULT_REGION,
-};
-
 const app = new App();
 
-new MyStack(app, 'my-stack-dev', { env: devEnv });
-// new MyStack(app, 'my-stack-prod', { env: prodEnv });
+new CertValidation(app, 'cetificate-validation');
 
 app.synth();
